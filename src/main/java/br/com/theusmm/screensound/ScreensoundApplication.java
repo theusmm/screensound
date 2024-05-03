af24609168a8f9b54c77bb97c6034df1fcd1ef6c
theusmm <theusmm75@gmail.com>
2024-05-03 19:37:13 -0300

    first commit

---
+++ src/main/java/br/com/theusmm/screensound/ScreensoundApplication.java
@@ -0,0 +1,25 @@
+package br.com.theusmm.screensound;
+
+import br.com.theusmm.screensound.principal.ArtistaRepository;
+import br.com.theusmm.screensound.principal.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreensoundApplication implements CommandLineRunner {
+
+	@Autowired
+	private ArtistaRepository repositorio;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreensoundApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repositorio);
+		principal.exibeMenu();
+	}
+}
---
+++ src/main/java/br/com/theusmm/screensound/principal/ArtistaRepository.java
@@ -0,0 +1,16 @@
+package br.com.theusmm.screensound.principal;
+
+import br.com.theusmm.screensound.model.Artista;
+import br.com.theusmm.screensound.model.Musica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ArtistaRepository extends JpaRepository<Artista, Long>{
+    Optional<Artista> findByNomeContainingIgnoreCase(String nome);
+
+    @Query("SELECT m FROM Artista a JOIN a.musicas m WHERE a.nome ILIKE %:nome%")
+    List<Musica> buscaMusicasPorArtista(String nome);
+}
